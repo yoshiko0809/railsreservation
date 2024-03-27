0cf97fdc1bda04674a9c9a1e9fea3a4c58286269
yoshiko-oka <164520252+yoshiko-oka@users.noreply.github.com>
2024-03-27 23:09:05 +0900

    ログイン機能実装

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,9 @@
 Rails.application.routes.draw do
-  devise_for :users
+  root 'pages#home'
+  get 'pages/home'
+  devise_for :users,
+  path: '',
+  path_names: {sign_up: 'register', sign_in: 'login', edit: 'profile', sign_out: 'logout'},
+  controllers: {registrations: 'registrations'}
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
